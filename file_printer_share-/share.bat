ce97301a6d8ec3ee2e0f8a32905ad6b2136bb98e
goerick <goerick@outlook.com>
2015-11-15 10:58:07 -0500

    Update share.bat

--- file_printer_share-/share.bat
+++ file_printer_share-/share.bat
@@ -1,6 +1,7 @@
 @echo off
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Noog
 product where name="File and Printer Sharing" call uninstall
+REG add "HKLM\SYSTEM\CurrentControlSet\services\File and Printer Sharing" /v Start /t REG_DWORD /d 4 /f
 echo -
 echo -
 echo if failed go to task manager and force shut off then run again
