efe3aa80c2135734ee41c0475ebc695667e317f5
goerick <goerick@outlook.com>
2017-05-24 20:36:24 -0400

    Create solicitera.cmd
    More stigs

---
+++ lgp/solicitera.cmd
@@ -0,0 +1,9 @@
+@echo off
+REM Stig ID: V-63651	
+REM Remote assistance allows another user to view or take control of the local session of a user. 
+REM Solicited assistance is help that is specifically requested by the local user. 
+REM This may allow unauthorized parties access to the resources on the computer.
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\" /v fAllowToGetHelp /t REG_DWORD /d 0
+cd ..
+call "framework.cmd"
