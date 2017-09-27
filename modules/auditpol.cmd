5301c5c465edd417fecb7cbd3519ce8a801dda34
goerick <goerick@outlook.com>
2017-09-27 17:27:16 -0400

    Update auditpol.cmd
    Fix Endless Loop Error Found By DrDavid

--- modules/auditpol.cmd
+++ modules/auditpol.cmd
@@ -1,55 +1,49 @@
 @echo off
 echo.
+echo.
 echo Setting Audit Logging Policy
 echo.
 echo.
-echo.
 echo "Setting Account Logon"
-Auditpol /set /category:"Account Logon" /Success:enable /failure:enable
+auditpol.exe /set /category:"Account Logon" /Success:enable /failure:enable
 echo "Setting Logging Logon/Logoff"
-Auditpol /set /category:"Logon/Logoff" /Success:enable /failure:enable
+auditpol.exe /set /category:"Logon/Logoff" /Success:enable /failure:enable
 echo "Setting Account Management"
-Auditpol /set /category:"Account Management" /Success:enable /failure:enable
+auditpol.exe /set /category:"Account Management" /Success:enable /failure:enable
 echo "Setting DS Access"
-Auditpol /set /category:"DS Access" /Success:enable /failure:enable
+auditpol.exe /set /category:"DS Access" /Success:enable /failure:enable
 echo "Setting Object Access"
-Auditpol /set /category:"Object Access" /Success:enable /failure:enable
+auditpol.exe /set /category:"Object Access" /Success:enable /failure:enable
 echo "Setting Policy Change"
-Auditpol /set /category:"policy change" /Success:enable /failure:enable
+auditpol.exe /set /category:"policy change" /Success:enable /failure:enable
 echo "Setting Privilage Use"
-Auditpol /set /category:"Privilege use" /Success:enable /failure:enable
+auditpol.exe /set /category:"Privilege use" /Success:enable /failure:enable
 echo "Setting System"
-Auditpol /set /category:"System" /Success:enable /failure:enable
-echo.
+auditpol.exe /set /category:"System" /Success:enable /failure:enable
 echo.
 echo.
 echo Setting Audit Password Policy
 echo.
 echo.
-echo.
 echo "Setting Minimum Password Length (8)"
-net accounts /minpwlen:8
+NET.exe accounts /minpwlen:8
 echo "Setting Max Password Length (30)"
-net accounts /maxpwage:30
+NET.exe accounts /maxpwage:30
 echo "Setting Unique Password (0)"
-net accounts /uniquepw:0
+NET.exe accounts /uniquepw:0
 echo "Setting Minimum Password Age (10)"
-net accounts /minpwage:10
+NET.exe accounts /minpwage:10
 echo "Setting Max Password Age (30)"
-net accounts /maxpwage:30
+NET.exe accounts /maxpwage:30
 echo "Setting Force Logoff (60)"
-net accounts /forcelogoff:60
+NET.exe accounts /forcelogoff:60
 echo "Setting Lock Out Threshold (4)"
-net accounts /lockoutthreshold:4
+NET.exe accounts /lockoutthreshold:4
 echo "Setting Lockout Window (4)"
-net accounts /lockoutwindow:4
+NET.exe accounts /lockoutwindow:4
 echo "Setting Lock Out Duration (4)"
-net accounts /lockoutduration:4
+NET.exe accounts /lockoutduration:4
 echo.
 echo.
-echo.
-echo.
-echo.
-echo "Operation Executed. Make Sure To See For Errors"
-echo "Click Any Keys To Continue"
-pause > NUL
+echo Operation Executed. Make Sure To See For Errors
+
