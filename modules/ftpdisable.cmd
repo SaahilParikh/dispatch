ee8d14309705e8fb8e75f10e73a7157f560ce111
goerick <goerick@outlook.com>
2017-09-30 14:03:57 -0400

    Update ftpdisable.cmd
    Made it abit more sophisticated but don't know if it works

--- modules/ftpdisable.cmd
+++ modules/ftpdisable.cmd
@@ -1,10 +1,21 @@
-@echo off
+
 echo.
-echo
-net stop msftpsvc
+echo Checking if ftp service exists...
+net stop msftpsvc >NUL
+IF %ERRORLEVEL% EQU 0 goto :cont || goto :dne
+
+
+:cont
 sc stop "MSFtpsvc"
 sc config "MSFtpsvc" start= disabled
 sc delete "MSFtpsvc"
 echo.
+goto :doneftp
+
+:dne 
+echo Seems like mftpsvc does not exist...
+goto :doneftp
+
+:doneftp
 echo Operation Executed
-cd ..
+
