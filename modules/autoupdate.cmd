6786ebe074811a91c86790711975b159c0efb66e
goerick <goerick@outlook.com>
2017-09-30 13:46:08 -0400

    Update autoupdate.cmd
    fixed autoupdate

--- modules/autoupdate.cmd
+++ modules/autoupdate.cmd
@@ -3,18 +3,12 @@ echo.
 echo.
 net stop wuauserv
 echo.
-Stopping Service...
-echo.
-echo.
 echo Configuring...
 sc config wuauserv start= auto
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 0 /f
 echo.
-echo.
 echo Restarting Service...
-echo.
 net start wuauserv
-echo.
 echo.
 echo Operation Executed
 echo.
