b8cefbf2bda2fe549df288e7d066d59a684ec37a
goerick <goerick@outlook.com>
2017-09-30 13:56:15 -0400

    Update firewall.cmd
    Made things a little more clear

--- modules/firewall.cmd
+++ modules/firewall.cmd
@@ -1,11 +1,23 @@
 @echo off
 echo.
-echo enabling firewall...
-netsh advfirewall set currentprofile state on
+echo Enabling firewall...
+echo.
+echo set currentprofile state on 
+netsh advfirewall set currentprofile state on > NUL
+echo.
+echo set currentprofile state on 
 netsh advfirewall set privateprofile state on
+echo.
+echo set domainprofile state on
 netsh advfirewall set domainprofile state on
+echo.
+echo advfirewall set publicprofile state on
 netsh advfirewall set publicprofile state on
-REM Just incase if it doesn't work
+echo.
+:: Just incase if it doesn't work
+echo. 
+echo set allprofiles state on
 netsh advfirewall set allprofiles state on
 echo.
 echo Operation Executed
+echo.
