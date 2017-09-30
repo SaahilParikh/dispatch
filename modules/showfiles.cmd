87eb9456b6d72925ffa1f71e8ad777fe8dccc1ec
goerick <goerick@outlook.com>
2017-09-30 14:55:20 -0400

    Update showfiles.cmd
    FIXED

--- modules/showfiles.cmd
+++ modules/showfiles.cmd
@@ -1,5 +1,11 @@
 @echo off
-REM Show Hidden Files
-chdir C:/
-attrib -s -h -r /s /d
+echo.
+echo Showing hidden files...
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REG_DWORD /d 1 /f
+IF %ERRORLEVEL% EQU 0 echo Operation Executed || echo An Error Occured
+echo.
+echo Show super hidden files...
+reg ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 1 /f
+IF %ERRORLEVEL% EQU 0 echo Operation Executed || echo An Error Occured
 echo Hidden Files And Directories Are Now Exposed!
+echo.
