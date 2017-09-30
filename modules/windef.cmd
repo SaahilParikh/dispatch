02c31c708e467e963a2d9b3239b8de0d72bbc659
goerick <goerick@outlook.com>
2017-09-30 18:26:53 -0400

    Update windef.cmd
    fixed

--- modules/windef.cmd
+++ modules/windef.cmd
@@ -1,13 +1,28 @@
-@echo off
 echo.
+
+if exist "%ProgramFiles%\Windows Defender\MpCmdRun.exe" (
+    :windef
+) else (
+    :missingdef
+)
+
+
+
+:windef
 echo.
 echo Restoring Windows Defender To Default Settings
+"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -RestoreDefaults
 echo.
-MpCmdRun.exe -RestoreDefaults
-echo.
+
 echo Updating Windows Defender
+"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -SignatureUpdate
 echo.
-MpCmdRun.exe -SignatureUpdate
+goto :done
+
+:missingdef
+echo MpCmdRun.exe is unavailable
 echo.
+
+:done
+echo Operation Executed
 echo.
-echo "Operation Executed"
