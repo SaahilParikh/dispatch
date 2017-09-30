8b2cb44ca40069812d2bfba02cfab84a58e8c8da
goerick <goerick@outlook.com>
2017-09-30 13:49:09 -0400

    Update ctrlaltdel.cmd
    fixed ctrlaltdel

--- modules/ctrlaltdel.cmd
+++ modules/ctrlaltdel.cmd
@@ -2,6 +2,5 @@
 echo.
 echo.
 reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableCAD /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\CurrentVersion\Policies\System" /f /v DisableCAD /t REG_WORD /d 1 /f
 echo.
 echo Operation Executed
