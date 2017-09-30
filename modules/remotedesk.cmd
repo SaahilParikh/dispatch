ce09f73585ed16687b49296a489f26bf5ea35a7f
goerick <goerick@outlook.com>
2017-09-30 18:32:55 -0400

    Update remotedesk.cmd

--- modules/remotedesk.cmd
+++ modules/remotedesk.cmd
@@ -1,32 +1,15 @@
 @echo off
 echo.
-echo.
-echo.
 
-setlocal
-if {%1}=={} goto :syntax
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 
-:loop
-if {%1}=={} goto :finish
-set remote="\\%1\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server"
-shift
-reg.exe ADD %remote% /v fDenyTSConnections /t REG_DWORD /d 1 /f>nul 2>&1
-if NOT %ERRORLEVEL% EQU 0 @echo %remote% NOT found.
-goto :done
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 
-:syntax
-@echo Syntax: RemoteDesktop Computer1 [Computer2 .... Computern]
-goto :loop
-
-:finish
-endlocal
 netsh advfirewall firewall set rule group="remote desktop" new enable=No profile=domain
 netsh advfirewall firewall set rule group="remote desktop" new enable=No profile=private
 netsh advfirewall firewall set rule group="remote desktop" new enable=No
-goto :done
 
-:done
-echo.
-echo.
 echo.
 echo Operation Executed
