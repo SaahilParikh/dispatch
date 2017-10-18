3130f3758f1f7ac1e0eb63e0529efdbe1bd3b117
goerick <goerick@outlook.com>
2017-10-17 21:30:12 -0400

    Update rpclocator.cmd

--- modules/rpclocator.cmd
+++ modules/rpclocator.cmd
@@ -1,6 +1,5 @@
 @echo off
 net stop RpcLocator
-product where name="RpcLocator" call uninstall
 sc delete RpcLocator
 REG add "HKLM\SYSTEM\CurrentControlSet\services\RpcLocator" /v Start /t REG_DWORD /d 4 /f
 echo Operation Executed
